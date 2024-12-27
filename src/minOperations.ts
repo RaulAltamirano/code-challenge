c361021c7903d88e22345174c32d8a9be4542715
Pacman <rulomania2011@hotmail.com>
2024-12-27 13:32:23 -0600

    create(minOperations) solved new challenge

--- src/minOperations.ts
+++ src/minOperations.ts
@@ -1,33 +1,40 @@
-function minOperations(nums: string | any[],queries: number[]) {
-    const res : number[] = []
-    for(let i = 0; i < nums.length; i++){
-        let sum = 0
-        let currentValue = nums[i]
-        for(let query of queries){
+function minOperations(nums: number[], queries: number[]): number[] {
+	nums.sort((a, b) => a - b);
 
-                if(currentValue > query){
-                    let ammount = currentValue
-                   do {
-                        ammount --
-                        sum ++
-                       console.log('Este es el ammount',ammount, query, 'del query', sum ,'sum', 'de', currentValue)
-                    } while (ammount > query)
-        res.push(sum)
+	const n = nums.length;
+	const prefixSum = new Array(n).fill(0);
+	prefixSum[0] = nums[0];
+	for (let i = 1; i < n; i++) {
+			prefixSum[i] = prefixSum[i - 1] + nums[i];
+	}
 
-                }
-                else if(currentValue < query){
-                    let ammount = currentValue
-                    do {
-                        ammount ++
-                        sum ++
-                        console.log('Este es el ammount',ammount, query, 'del query', sum ,'sum', 'de', currentValue)
-                    } while (ammount < query)
-                            res.push(sum)
+	const results = new Array(queries.length);
+	for (let i = 0; i < queries.length; i++) {
+			const query = queries[i];
+			const idx = binarySearch(nums, query);
+			const leftCost = idx > 0 ? query * idx - prefixSum[idx - 1] : 0;
+			const rightCost =
+					idx < n
+							? prefixSum[n - 1] - (idx > 0 ? prefixSum[idx - 1] : 0) - query * (n - idx)
+							: 0;
+			results[i] = leftCost + rightCost;
+	}
 
-                }
-        }
-    }
-    return res
-};
+	return results;
+}
 
-minOperations([3,1,6,8], [1,5] )
+function binarySearch(nums: number[], target: number): number {
+	let low = 0, high = nums.length;
+	while (low < high) {
+			const mid = Math.floor((low + high) / 2);
+			if (nums[mid] < target) {
+					low = mid + 1;
+			} else {
+					high = mid;
+			}
+	}
+	return low;
+}
+
+
+minOperations([3, 1, 6, 8], [1, 5])
