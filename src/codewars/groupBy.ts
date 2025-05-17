125d82643043b220153f6d6fdfe589d369bf3be0
Pacman <rulomania2011@hotmail.com>
2025-05-17 15:21:58 -0600

    create(groupBy) create new exersice

---
+++ src/codewars/groupBy.ts
@@ -0,0 +1,26 @@
+const employees = [
+    { name: "James", income: 1000, profession: "developer", age: 23, },
+    { name: "Robert", income: 1100, profession: "qa", age: 34, },
+    { name: "John", income: 1200, profession: "designer", age: 32, },
+    { name: "Mary", income: 1300, profession: "designer", age: 22, },
+    { name: "Patricia", income: 1400, profession: "qa", age: 23, },
+    { name: "Jennifer", income: 1500, profession: "developer", age: 45, },
+    { name: "Max", income: 1600, profession: "developer", age: 27, },
+];
+
+
+
+const result = Map()
+const profession = { profession: true };
+
+
+
+export function groupBy(
+    array: unknown,
+    classifier: unknown,
+    downstream: unknown,
+    accumulatorSupplier: unknown,
+): any {
+
+
+}
