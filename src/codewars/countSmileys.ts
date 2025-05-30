a753748ad6d70c1c780111ec54235d25fa35ba9a
Pacman <rulomania2011@hotmail.com>
2025-05-30 15:08:45 -0600

    create(countSmileys) solved new exersice

---
+++ src/codewars/countSmileys.ts
@@ -0,0 +1,25 @@
+export function countSmileys(arr: string[]): number {
+    if (!arr.length) return 0;
+
+    let count = 0;
+    
+    for (const face of arr) {
+        const len = face.length;
+        
+        if (len !== 2 && len !== 3) continue;
+        
+        if (!(face[0] === ':' || face[0] === ';')) continue;
+        
+        if (!(face[len - 1] === ')' || face[len - 1] === 'D')) continue;
+        
+        if (len === 3 && !(face[1] === '-' || face[1] === '~')) continue;
+        
+        count++;
+    }
+    console.log(count);
+    return count;
+}
+
+
+countSmileys([':D', ':~)', ';~D', ':)'])
+countSmileys([';]', ':[', ';*', ':$', ';-D']); 
